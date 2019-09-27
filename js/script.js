352eeba08f2164ef5b61513765a08abde8de96f9
vahanbarseghyan21 <51117466+vahanbarseghyan21@users.noreply.github.com>
2019-09-27 22:06:53 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullPage', {
+	autoScrolling: true,
+	navigation: true,
+	anchors: ['section1', 'section2', 'section3'],
+	navigationTooltips: ['Home', 'About us', 'Contact'],
+	showActiveTooltip: true,
+	scrollingSpeed: 1500,
+	controlArrows: false,
+	slidesNavigation: true
+});
